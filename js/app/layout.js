cea6d479d1f63276e12a9db7e4d9cecea6e6c76b
Carl Bird <alevel5pidgey@gmail.com>
2026-09-03 16:57:43 +1000

    Fixed: Error preventing fish timers updating

--- js/app/layout.js
+++ js/app/layout.js
@@ -222,8 +222,6 @@ class FishTableLayout {
         .text((fishEntry.uptime * 100.0).toFixed(1));
     }
 
-    ViewModel.updateCalendarActionState($fishEntry, fishEntry);
-
     // Omit this if the fish doesn't have limited availability.. obviously...
     if (hasLimitedAvailability) {
       // Set the "current availability" time. Remember, we've cached the other
